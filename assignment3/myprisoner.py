db2459548a7aaceebd44d7a91b12359ff9fe1499
erinius <timme003@cougars.csusm.edu>
2021-10-28 21:18:19 +0000

    Added basic argparse stuff from lecture

--- assignment3/myprisoner.py
+++ assignment3/myprisoner.py
@@ -1,8 +1,14 @@
 #my code goes here, this is the base file
 import json
-
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', help='called when new game')
+    parser.add_argument('--iterations', help='number of iterations in game')
+    parser.add_argument('--last_opponent_move', help='last opponent move')
+
+    args = parser.parse_args()
     return
 
 
