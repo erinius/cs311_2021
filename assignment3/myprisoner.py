726d943e0c5e883b10d0c8a9c1c72a14f442ccde
erinius <timme003@cougars.csusm.edu>
2021-11-01 11:42:52 +0000

    made working prisoner's dilemma program

--- assignment3/myprisoner.py
+++ assignment3/myprisoner.py
@@ -1,6 +1,19 @@
-#my code goes here, this is the base file
+#By Eric Timmerman
+#I'm using a list as a queue which isn't very efficient, cause you can't exactly save a queue in json
+#Strategy is what I've decided to call "disproportionate retribution"
 import json
 import argparse
+import queue as q
+import io
+
+
+
+def init(moves):
+    moves.append("silent")
+    moves.append("silent")
+    moves.append("silent")
+    moves.append("confess")
+    return
 
 def main():
     parser = argparse.ArgumentParser()
@@ -9,8 +22,27 @@ def main():
     parser.add_argument('--last_opponent_move', help='last opponent move')
 
     args = parser.parse_args()
+
+    moves = []
+
     if args.init == 'true':
-        print("init is true")
+        init(moves)
+    else:
+        read = io.open("data.json", "r")
+        moves = json.load(read)
+
+        if args.last_opponent_move == "confess":
+            moves.append("confess")
+            moves.append("confess")
+        elif args.last_opponent_move == "silent":
+            moves.append("silent")
+    
+    move = moves.pop(0)
+
+    save = io.open("data.json", "w")
+    json.dump(moves, save)
+
+    print(move)
     return
 
 
